9fe046d32ddc9a7e8b882c244ca688d8fbae21a9
Natacha Bagnard <>
2021-12-23 14:08:18 -0800

    Modify default integration tests and only provide a connection test for now

--- odbc/tests/common/mod.rs
+++ odbc/tests/common/mod.rs
@@ -1,34 +1,6 @@
-extern crate odbc_api;
-
-use self::odbc_api::handles::StatementImpl;
-use self::odbc_api::{Connection, Cursor, CursorImpl, Environment};
 use lazy_static::lazy_static;
-use odbc_api::ResultSetMetadata;
+use odbc_api::{Connection, Environment, Error};
 use std::env;
-
-#[derive(Debug, PartialEq)]
-pub struct ResultsetMetadata {
-    pub expected_sql_types: Vec<String>,
-    pub expected_bson_type: Vec<String>,
-    pub expected_catalog_name: Vec<String>,
-    pub expected_column_class_name: Vec<String>,
-    pub expected_column_display_size: Vec<String>,
-    pub expected_column_label: Vec<String>,
-    pub expected_column_type: Vec<String>,
-    pub expected_precision: Vec<String>,
-    pub expected_scale: Vec<String>,
-    pub expected_schema_name: Vec<String>,
-    pub expected_is_auto_increment: Vec<String>,
-    pub expected_is_case_sensitive: Vec<String>,
-    pub expected_is_currency: Vec<String>,
-    pub expected_is_definitely_writable: Vec<String>,
-    pub expected_is_nullable: Vec<String>,
-    pub expected_is_read_only: Vec<String>,
-    pub expected_is_searchable: Vec<String>,
-    pub expected_is_signed: Vec<String>,
-    pub expected_is_writable: Vec<String>,
-    pub expected_names: Vec<String>,
-}
 
 // Allocate a new environment handle.
 // Most tests will only need one and this should be part of the setup mechanism.
@@ -36,10 +8,11 @@ lazy_static! {
     pub static ref ODBC_ENV: Environment = Environment::new().unwrap();
 }
 
-#[allow(dead_code)]
-/// Connect to the given Driver or DSN with the provided uid, pwd and host.
-/// The default auth db is 'admin' is not specified.
-pub fn connect() -> Connection<'static> {
+/// Generate the default connection setting defined for the tests using a connection string
+/// of the form 'Driver={};PWD={};USER={};SERVER={};AUTH_SRC={}'.
+/// The default driver is 'ADL_ODBC_DRIVER' if not specified.
+/// The default auth db is 'admin' if not specified.
+fn generate_default_connection_str() -> String {
     let user_name = env::var("ADL_TEST_USER").expect("ADL_TEST_USER is not set");
     let password = env::var("ADL_TEST_PWD").expect("ADL_TEST_PWD is not set");
     let host = env::var("ADL_TEST_HOST").expect("ADL_TEST_HOST is not set");
@@ -55,9 +28,6 @@ pub fn connect() -> Connection<'static> {
         Err(_e) => "ADL_ODBC_DRIVER".to_string(), //Default driver name
     };
 
-    let _dsn = env::var("ADL_TEST_DSN");
-    // TODO : If DSN is specified, it should take over using 'DRIVER=' for connecting and use 'DSN='.
-
     let mut connection_string = format!(
         "Driver={};PWD={};USER={};SERVER={};AUTH_SRC={}",
         driver, user_name, password, host, auth_db,
@@ -69,43 +39,13 @@ pub fn connect() -> Connection<'static> {
         Err(_e) => (), // Do nothing
     };
 
-    ODBC_ENV
-        .connect_with_connection_string(&connection_string)
-        .unwrap()
+    connection_string
 }
 
-#[allow(dead_code)]
-/// Validate the resultset and its metadata
-pub fn validate_rs(
-    expected_row_count_opt: Option<i64>,
-    expected_result_set_opt: Option<&Vec<Vec<String>>>,
-    expected_rs_meta_opt: Option<&ResultsetMetadata>,
-    rs_cursor: &mut CursorImpl<StatementImpl>,
-) {
-    // Validate metadata
-    if let Some(expected_rs_meta) = expected_rs_meta_opt {
-        let num_col = rs_cursor.num_result_cols().unwrap();
-        assert_eq!(expected_rs_meta.expected_names.len() as i16, num_col);
-        let column_names = rs_cursor.column_names().unwrap();
-        for (index, name) in column_names.enumerate() {
-            assert_eq!(expected_rs_meta.expected_names[index], name.unwrap());
-        }
+/// Connect using the given connection string or the default settings if no connection string are provided.
+pub fn connect(connection_string: Option<&str>) -> Result<Connection<'_>, Error> {
+    match connection_string {
+        Some(str) => ODBC_ENV.connect_with_connection_string(str),
+        None => ODBC_ENV.connect_with_connection_string(generate_default_connection_str().as_str()),
     }
-
-    if expected_row_count_opt.is_some() || expected_result_set_opt.is_some() {
-        let mut row_count: i64 = 0;
-        while let Ok(_row) = rs_cursor.next_row() {
-            row_count += 1;
-            // TODO - Validate RS content
-        }
-
-        // Validate row count
-        if expected_row_count_opt.is_some() {
-            assert_eq!(expected_row_count_opt.unwrap(), row_count)
-        }
-    }
-
-    // TODO Do we need to clean-up? sqlCloseCursor? sqlFreeStatement?
-    // The caller will have a clean-up session which call sqlDisconnect
-    // and should free all the statements attached to the connection
 }
---
+++ odbc/tests/connection_tests.rs
@@ -0,0 +1,24 @@
+mod common;
+use common::connect;
+
+#[test]
+fn test_invalid_connection() {
+    // Invalid Driver name
+    let conn_str = "Driver=NotAnAvailableDriver;PWD=N/A;USER=N/A;SERVER=N/A;AUTH_SRC=N/A";
+    let result = connect(Some(conn_str));
+    assert!(
+        result.is_err(),
+        "The connection should have failed, but it was successful."
+    );
+}
+#[allow(dead_code)]
+//#[test]
+// Uncomment to verify that driver is installed.
+// It will still fail until SQLDriverConnect is implemented, but it will show that the DM found the driver.
+fn test_default_connection() {
+    let result = connect(None);
+    assert!(
+        result.is_ok(),
+        "The connection should have succeeded, but it failed."
+    );
+}
