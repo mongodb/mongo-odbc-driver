4e1144d65890ba39c2df4060ee2494930ccd323a
Natacha Bagnard <>
2022-01-14 10:58:12 -0800

    Export only necessary items

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,11 +1,17 @@
-pub mod collections;
-pub mod conn;
-pub mod databases;
+mod collections;
+pub use collections::MongoCollections;
+mod conn;
+pub use conn::MongoConnection;
+mod databases;
+pub use databases::MongoDatabases;
 mod err;
-pub mod fields;
-pub mod query;
-pub mod stmt;
+mod fields;
+pub use fields::MongoFields;
+mod query;
+pub use query::{MongoColMetadata, MongoQuery};
+mod stmt;
 pub use err::{Error, Result};
+pub use stmt::MongoStatement;
 
 #[cfg(test)]
 mod tests {
