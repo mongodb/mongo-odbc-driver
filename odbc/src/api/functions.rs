50cfcf9cf17d430f4bd46209d6aca92fc44b6512
Patrick Meredith <pmeredit@gmail.com>
2022-07-30 22:36:03 -0400

    SQL-634: Ensure it's the code causing the failure somehow

--- odbc/src/api/functions.rs
+++ odbc/src/api/functions.rs
@@ -399,6 +399,7 @@ pub extern "C" fn SQLDriverConnectW(
     _driver_completion: DriverConnectOption,
 ) -> SqlReturn {
     println!("______");
+    return SqlReturn::SUCCESS;
     let conn_handle = MongoHandleRef::from(connection_handle);
     let conn = (*conn_handle).as_connection();
     if conn.is_none() {
