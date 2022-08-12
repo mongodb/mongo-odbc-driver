dbcc791453cbb0cd5f86f3c196d91d976061e8d4
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2022-08-12 13:48:29 -0400

    SQL-982: Add example code for inserting into mongodb

--- integration-test/src/main.rs
+++ integration-test/src/main.rs
@@ -1,4 +1,5 @@
 use lazy_static::lazy_static;
+use mongodb::sync::Client;
 use serde::{Serialize, Deserialize};
 
 lazy_static! {
@@ -22,7 +23,18 @@ struct TestDataEntry {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let f = std::fs::File::open(TEST_DATA_DIRECTORY.clone())?;
+
     let test_data: TestData = serde_yaml::from_reader(f)?;
     println!("Read YAML file: {:?}", test_data);
+
+    let client = Client::with_uri_str("mongodb://localhost:27017")?;
+
+    for e in test_data.dataset {
+        let db = client.database(e.db.as_str());
+        let collection = db.collection::<mongodb::bson::Bson>(e.collection.clone().unwrap().as_str());
+        let res = collection.insert_many(e.docs.unwrap(), None)?;
+        println!("insert result: {:?}", res);
+    }
+
     Ok(())
 }
