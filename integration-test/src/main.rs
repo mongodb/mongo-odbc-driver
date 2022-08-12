6ab24764f40ae93d1f60b8a7da2e54853baa9212
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2022-08-12 11:54:53 -0400

    SQL-982: Add basic code for parsing data yaml files

--- integration-test/src/main.rs
+++ integration-test/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("TODO: data loader");
+use lazy_static::lazy_static;
+use serde::{Serialize, Deserialize};
+
+lazy_static! {
+    pub static ref TEST_DATA_DIRECTORY: String = "resources/integration_test/testdata/integration_test.yml".to_string();
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TestData {
+    dataset: Vec<TestDataEntry>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TestDataEntry {
+    db: String,
+    collection: Option<String>,
+    view: Option<String>,
+    docs: Option<Vec<mongodb::bson::Bson>>,
+    schema: Option<mongodb::bson::Bson>,
+    indexes: Option<mongodb::bson::Bson>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let f = std::fs::File::open(TEST_DATA_DIRECTORY.clone())?;
+    let test_data: TestData = serde_yaml::from_reader(f)?;
+    println!("Read YAML file: {:?}", test_data);
+    Ok(())
 }
