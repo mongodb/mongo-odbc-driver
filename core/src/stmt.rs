6a7aa1945da47e7d5cb47863e8c7c3ca48081da1
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2022-09-19 14:35:52 -0700

    Add Debug as super trait to MongoStatement

--- core/src/stmt.rs
+++ core/src/stmt.rs
@@ -2,17 +2,11 @@ use crate::err::Result;
 use bson::Bson;
 use std::fmt::Debug;
 
-pub trait MongoStatement {
+pub trait MongoStatement: Debug {
     // Move the cursor to the next item.
     // Return true if moving was successful, false otherwise.
     fn next(&mut self) -> Result<bool>;
     // Get the BSON value for the cell at the given colIndex on the current row.
     // Fails if the first row has not been retrieved (next must be called at least once before getValue).
     fn get_value(&self, col_index: u16) -> Result<Option<Bson>>;
-}
-
-impl Debug for dyn MongoStatement {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "MongoStatement")
-    }
 }
