bc767b912ece42dce6969974abb5accf109b92c8
Patrick Meredith <pmeredit@protonmail.com>
2022-10-04 15:32:36 -0400

    Update core/src/fields.rs
    Co-authored-by: Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>

--- core/src/fields.rs
+++ core/src/fields.rs
@@ -156,8 +156,7 @@ mod unit {
     #[test]
     fn metadata_column_names() {
         use crate::{fields::MongoFields, stmt::MongoStatement};
-        // These were generated straight from the docs (hence the - 1). This
-        // gives us assurance that the column names are all correct.
+        // This gives us assurance that the column names are all correct.
         assert_eq!(
             "TABLE_CAT",
             MongoFields::empty().get_col_metadata(1).unwrap().col_name
