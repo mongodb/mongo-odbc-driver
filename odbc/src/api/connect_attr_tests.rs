0509c9f5705c8a8cfba75948c3cb9ef126b63fc5
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2022-10-13 15:42:29 -0400

    SQL-654: Add set tests

--- odbc/src/api/connect_attr_tests.rs
+++ odbc/src/api/connect_attr_tests.rs
@@ -3,13 +3,13 @@ mod unit {
         errors::ODBCError,
         handles::definitions::{Connection, ConnectionState, MongoHandle},
         util::input_wtext_to_string,
-        SQLGetConnectAttrW,
+        SQLGetConnectAttrW, SQLSetConnectAttrW,
     };
-    use odbc_sys::{ConnectionAttribute, Integer, SqlReturn, UInteger};
+    use odbc_sys::{ConnectionAttribute, Integer, Pointer, SqlReturn, UInteger};
     use std::sync::RwLock;
 
     // Test getting CurrentCatalog attribute from the Connection
-    // when none of the attributes are explicitly set.
+    // when none of the attributes are set.
     #[test]
     fn get_string_attrs_default() {
         unsafe {
@@ -65,7 +65,7 @@ mod unit {
     }
 
     // Test getting numeric attributes from the Connection
-    // when none of the attributes are explicitly set.
+    // when none of the attributes are set.
     #[test]
     fn get_numeric_attrs_default() {
         unsafe {
@@ -114,10 +114,10 @@ mod unit {
         }
     }
 
+    // Test getting invalid attributes.
     #[test]
     fn get_invalid_attr() {
         unsafe {
-            let value_ptr: *mut std::ffi::c_void = Box::into_raw(Box::new([0u8; 40])) as *mut _;
             let out_length = &mut 10;
 
             for attr in [
@@ -145,13 +145,19 @@ mod unit {
                 let mongo_handle: *mut _ = &mut MongoHandle::Connection(RwLock::new(conn));
                 assert_eq!(
                     SqlReturn::ERROR,
-                    SQLGetConnectAttrW(mongo_handle as *mut _, attr, value_ptr, 0, out_length)
+                    SQLGetConnectAttrW(
+                        mongo_handle as *mut _,
+                        attr,
+                        std::ptr::null_mut() as Pointer,
+                        0,
+                        out_length
+                    )
                 );
                 // Check the actual
                 let conn_handle = (*mongo_handle).as_connection().unwrap();
                 let errors = &conn_handle.read().unwrap().errors;
                 assert_eq!(1, errors.len());
-                let actual_err = errors.last().unwrap();
+                let actual_err = errors.first().unwrap();
                 match actual_err {
                     ODBCError::InvalidAttrIdentifier(actual_attr) => assert_eq!(attr, *actual_attr),
                     _ => panic!("unexpected err: {:?}", actual_err),
@@ -160,10 +166,116 @@ mod unit {
         }
     }
 
-    // TODO: test setting
-    //   - errors:
-    //     - set invalid attribute
-    //     - set invalid value (for login_timeout only)
-    //   - set catalog
-    //   - set login_timeout
+    // Test setting CurrentCatalog attribute.
+    #[test]
+    fn set_string_attrs() {
+        unsafe {
+            let conn = Connection::with_state(std::ptr::null_mut(), ConnectionState::Connected);
+            let mongo_handle: *mut _ = &mut MongoHandle::Connection(RwLock::new(conn));
+
+            let mut value = "test".encode_utf16().collect::<Vec<u16>>();
+            value.push('\u{0}' as u16);
+            let buffer_length: Integer = 4;
+
+            assert_eq!(
+                SqlReturn::SUCCESS,
+                SQLSetConnectAttrW(
+                    mongo_handle as *mut _,
+                    ConnectionAttribute::CurrentCatalog,
+                    value.as_ptr() as Pointer,
+                    buffer_length,
+                )
+            );
+            let conn_handle = (*mongo_handle).as_connection().unwrap();
+            let attributes = &conn_handle.read().unwrap().attributes;
+            assert_eq!(attributes.current_catalog, Some("test".to_string()));
+        }
+    }
+
+    // Test setting LoginTimeout and ConnectionTimeout attributes.
+    #[test]
+    fn set_numeric_attrs() {
+        unsafe {
+            let conn = Connection::with_state(std::ptr::null_mut(), ConnectionState::Connected);
+            let mongo_handle: *mut _ = &mut MongoHandle::Connection(RwLock::new(conn));
+
+            let login_timeout_value: UInteger = 42u32;
+            let connection_timeout_value: UInteger = 24u32;
+
+            assert_eq!(
+                SqlReturn::SUCCESS,
+                SQLSetConnectAttrW(
+                    mongo_handle as *mut _,
+                    ConnectionAttribute::LoginTimeout,
+                    login_timeout_value as Pointer,
+                    0,
+                )
+            );
+            assert_eq!(
+                SqlReturn::SUCCESS,
+                SQLSetConnectAttrW(
+                    mongo_handle as *mut _,
+                    ConnectionAttribute::ConnectionTimeout,
+                    connection_timeout_value as Pointer,
+                    0,
+                )
+            );
+            let conn_handle = (*mongo_handle).as_connection().unwrap();
+            let attributes = &conn_handle.read().unwrap().attributes;
+            // We do support setting LoginTimeout
+            assert_eq!(attributes.login_timeout, Some(42));
+            // We do not support setting ConnectionTimeout
+            assert_eq!(attributes.connection_timeout, None);
+        }
+    }
+
+    // Test setting invalid attributes.
+    #[test]
+    fn set_invalid_attr() {
+        unsafe {
+            for attr in [
+                ConnectionAttribute::AsyncEnable,
+                ConnectionAttribute::AccessMode,
+                ConnectionAttribute::AutoCommit,
+                ConnectionAttribute::Trace,
+                ConnectionAttribute::TraceFile,
+                ConnectionAttribute::TranslateLib,
+                ConnectionAttribute::TranslateOption,
+                ConnectionAttribute::TxnIsolation,
+                ConnectionAttribute::OdbcCursors,
+                ConnectionAttribute::QuietMode,
+                ConnectionAttribute::PacketSize,
+                ConnectionAttribute::DisconnectBehaviour,
+                ConnectionAttribute::AsyncDbcFunctionsEnable,
+                ConnectionAttribute::AsyncDbcEvent,
+                ConnectionAttribute::EnlistInDtc,
+                ConnectionAttribute::EnlistInXa,
+                ConnectionAttribute::ConnectionDead,
+                ConnectionAttribute::AutoIpd,
+                ConnectionAttribute::MetadataId,
+            ] {
+                let conn = Connection::with_state(std::ptr::null_mut(), ConnectionState::Connected);
+                let mongo_handle: *mut _ = &mut MongoHandle::Connection(RwLock::new(conn));
+
+                assert_eq!(
+                    SqlReturn::ERROR,
+                    SQLSetConnectAttrW(
+                        mongo_handle as *mut _,
+                        attr,
+                        std::ptr::null_mut() as Pointer,
+                        0
+                    )
+                );
+                // Check the actual
+                let conn_handle = (*mongo_handle).as_connection().unwrap();
+                let errors = &conn_handle.read().unwrap().errors;
+                assert_eq!(1, errors.len());
+                let actual_err = errors.first().unwrap();
+                match actual_err {
+                    ODBCError::InvalidAttrIdentifier(actual_attr) => assert_eq!(attr, *actual_attr),
+                    _ => panic!("unexpected err: {:?}", actual_err),
+                }
+            }
+        }
+    }
 }
