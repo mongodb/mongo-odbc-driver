2b3f4c2c5e857104b5c3028ca4485ff61629d53c
Natacha Bagnard <natacha.bagnard@mongodb.com>
2022-12-05 10:20:32 -0800

    Move setup and connection code to common because it will be used by all flows tests

--- integration_test/tests/power_bi_tests.rs
+++ integration_test/tests/power_bi_tests.rs
@@ -1,26 +1,23 @@
 mod common;
 
 mod integration {
-    use crate::common::{generate_default_connection_str, setup};
-    use odbc::ffi::SQL_NTS;
     use odbc_sys::{
-        ConnectionAttribute, DriverConnectOption, HDbc, HEnv, Handle, HandleType, Pointer,
-        SQLAllocHandle, SQLDriverConnectW, SQLFreeHandle, SQLSetConnectAttrW, SmallInt, SqlReturn,
+        HDbc, HEnv, Handle, HandleType, InfoType, Pointer, SQLFreeHandle, SQLGetInfoW, SmallInt,
+        SqlReturn,
     };
-    use std::ptr::null_mut;
     use std::slice;
 
     /// Test PowerBI Setup flow
     #[test]
     fn test_setup() {
-        setup();
+        crate::common::setup();
     }
 
     /// Test PowerBi environment clean-up
     #[test]
     fn test_env_cleanup() {
         // We need a handle to be able to test that freeing the handle work
-        let env_handle: HEnv = setup();
+        let env_handle: HEnv = crate::common::setup();
 
         unsafe {
             // Verify that freeing the handle is working as expected
@@ -41,60 +38,12 @@ mod integration {
     /// - SQLGetInfoW(SQL_DBMS_VER)
     #[test]
     fn test_connection() {
-        let env_handle: HEnv = setup();
+        let env_handle: HEnv = crate::common::setup();
+        let (conn_handle, in_connection_string, out_connection_string, output_len) =
+            crate::common::power_bi_connect(env_handle);
 
         unsafe {
-            // Allocate a DBC handle
-            let mut dbc: Handle = null_mut();
-            assert_eq!(
-                SqlReturn::SUCCESS,
-                SQLAllocHandle(
-                    HandleType::Dbc,
-                    env_handle as *mut _,
-                    &mut dbc as *mut Handle
-                )
-            );
-
-            // Set the login timeout
-            let login_timeout = 15;
-            assert_eq!(
-                SqlReturn::SUCCESS,
-                SQLSetConnectAttrW(
-                    dbc as HDbc,
-                    ConnectionAttribute::LoginTimeout,
-                    login_timeout as Pointer,
-                    0,
-                )
-            );
-
-            // Generate the connection string and add a null terminator because PowerBi uses SQL_NTS for the length
-            let in_connection_string = generate_default_connection_str();
-            let mut in_connection_string_encoded: Vec<u16> =
-                in_connection_string.encode_utf16().collect();
-            in_connection_string_encoded.push(0);
-
-            let string_length_2 = &mut 0;
-            const BUFFER_LENGTH: SmallInt = 300;
-            let out_connection_string = &mut [0u16; (BUFFER_LENGTH as usize - 1)] as *mut _;
-
-            assert_ne!(
-                SqlReturn::ERROR,
-                SQLDriverConnectW(
-                    dbc as HDbc,
-                    null_mut(),
-                    in_connection_string_encoded.as_ptr(),
-                    SQL_NTS,
-                    out_connection_string,
-                    BUFFER_LENGTH,
-                    string_length_2,
-                    DriverConnectOption::NoPrompt,
-                )
-            );
-
             let input_len = in_connection_string.len() as SmallInt;
-            let output_len = *string_length_2;
-            let outputstr =
-                slice::from_raw_parts(out_connection_string, output_len as usize).to_vec();
 
             println!(
                 "Input connection string = {}\nLength is {}",
@@ -102,11 +51,66 @@ mod integration {
             );
             println!(
                 "Output connection string = {}\nLength is {}",
-                String::from_utf16_lossy(&outputstr),
-                output_len
+                out_connection_string, output_len
             );
             // The output string should be the same as the input string except with extra curly braces around the driver name
             assert_eq!(input_len, output_len, "Expect that both connection the input connection string and ouptput connection string have the same length but input string length is {} and output string length is {}",input_len, output_len);
+
+            let str_len_ptr = &mut 0;
+            const BUFFER_LENGTH: SmallInt = 300;
+            let output_buffer = &mut [0u16; (BUFFER_LENGTH as usize - 1)] as *mut _;
+
+            // SQL_DRIVER_NAME is not accessible through odbc_sys
+            /*
+            assert_eq!(
+                SqlReturn::SUCCESS,
+                SQLGetInfoW(
+                    dbc as HDbc,
+                    SQL_DRIVER_NAME, // 6
+                    driver_name as Pointer,
+                    BUFFER_LENGTH,
+                    str_len_ptr
+                )
+            );
+             */
+
+            assert_eq!(
+                SqlReturn::SUCCESS,
+                SQLGetInfoW(
+                    conn_handle as HDbc,
+                    InfoType::DbmsName,
+                    output_buffer as Pointer,
+                    BUFFER_LENGTH,
+                    str_len_ptr
+                )
+            );
+            println!(
+                "DBMS name = {}\nLength is {}",
+                String::from_utf16_lossy(slice::from_raw_parts(
+                    output_buffer,
+                    *str_len_ptr as usize
+                )),
+                *str_len_ptr
+            );
+
+            assert_eq!(
+                SqlReturn::SUCCESS,
+                SQLGetInfoW(
+                    conn_handle as HDbc,
+                    InfoType::DbmsVer,
+                    output_buffer as Pointer,
+                    BUFFER_LENGTH,
+                    str_len_ptr
+                )
+            );
+            println!(
+                "DBMS version = {}\nLength is {}",
+                String::from_utf16_lossy(slice::from_raw_parts(
+                    output_buffer,
+                    *str_len_ptr as usize
+                )),
+                *str_len_ptr
+            );
         }
     }
 }
