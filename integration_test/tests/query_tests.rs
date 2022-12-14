ea8c153c9e2809411ae7aedfd63b544d6ff4d356
Patrick Meredith <pmeredit@gmail.com>
2022-12-14 09:49:26 -0500

    SQL-1056: Add filter tests

--- integration_test/tests/query_tests.rs
+++ integration_test/tests/query_tests.rs
@@ -3,7 +3,7 @@ mod common;
 mod integration {
 
     #[test]
-    fn test_odbc_query() {
+    fn query() {
         use odbc::*;
         let env = create_environment_v3().unwrap();
         let mut conn_string = crate::common::generate_default_connection_str();
@@ -31,7 +31,7 @@ mod integration {
     }
 
     #[test]
-    fn ffftest_columns() {
+    fn all_columns() {
         use odbc_api::*;
         let mut conn_string = crate::common::generate_default_connection_str();
         conn_string.push_str("DATABASE=integration_test");
@@ -40,15 +40,87 @@ mod integration {
         let conn = env.connect_with_connection_string(&conn_string).unwrap();
 
         let mut cursor = conn.columns("", "", "", "");
+
+        let expected = [
+            ["integration_test", "example", "_id"],
+            ["integration_test", "example", "b"],
+            ["integration_test", "foo", "_id"],
+            ["integration_test", "foo", "a"],
+            ["integration_test_2", "example_2", "_id"],
+            ["integration_test_2", "example_2", "b"],
+        ];
+        let mut i = 0;
         while let Ok(Some(mut row)) = cursor.as_mut().unwrap().next_row() {
             let mut buf = Vec::new();
-            println!("NEXT");
+            let expected_row = expected[i];
+            i += 1;
             row.get_text(1, &mut buf).unwrap();
-            println!("DB NAME: {}", std::str::from_utf8(&buf).unwrap());
+            assert_eq!(expected_row[0], std::str::from_utf8(&buf).unwrap());
             row.get_text(3, &mut buf).unwrap();
-            println!("TABLE NAME: {}", std::str::from_utf8(&buf).unwrap());
+            assert_eq!(expected_row[1], std::str::from_utf8(&buf).unwrap());
             row.get_text(4, &mut buf).unwrap();
-            println!("COL NAME: {}", std::str::from_utf8(&buf).unwrap());
+            assert_eq!(expected_row[2], std::str::from_utf8(&buf).unwrap());
+        }
+    }
+
+    #[test]
+    fn columns_with_column_filter() {
+        use odbc_api::*;
+        let mut conn_string = crate::common::generate_default_connection_str();
+        conn_string.push_str("DATABASE=integration_test");
+
+        let env = Environment::new().unwrap();
+        let conn = env.connect_with_connection_string(&conn_string).unwrap();
+
+        let mut cursor = conn.columns("", "", "", "%i%");
+
+        let expected = [
+            ["integration_test", "example", "_id"],
+            ["integration_test", "foo", "_id"],
+            ["integration_test_2", "example_2", "_id"],
+        ];
+        let mut i = 0;
+        while let Ok(Some(mut row)) = cursor.as_mut().unwrap().next_row() {
+            let mut buf = Vec::new();
+            let expected_row = expected[i];
+            i += 1;
+            row.get_text(1, &mut buf).unwrap();
+            assert_eq!(expected_row[0], std::str::from_utf8(&buf).unwrap());
+            row.get_text(3, &mut buf).unwrap();
+            assert_eq!(expected_row[1], std::str::from_utf8(&buf).unwrap());
+            row.get_text(4, &mut buf).unwrap();
+            assert_eq!(expected_row[2], std::str::from_utf8(&buf).unwrap());
+        }
+    }
+
+    #[test]
+    fn columns_with_collection_filter() {
+        use odbc_api::*;
+        let mut conn_string = crate::common::generate_default_connection_str();
+        conn_string.push_str("DATABASE=integration_test");
+
+        let env = Environment::new().unwrap();
+        let conn = env.connect_with_connection_string(&conn_string).unwrap();
+
+        let mut cursor = conn.columns("", "", "%mp%", "");
+
+        let expected = [
+            ["integration_test", "example", "_id"],
+            ["integration_test", "example", "b"],
+            ["integration_test_2", "example_2", "_id"],
+            ["integration_test_2", "example_2", "b"],
+        ];
+        let mut i = 0;
+        while let Ok(Some(mut row)) = cursor.as_mut().unwrap().next_row() {
+            let mut buf = Vec::new();
+            let expected_row = expected[i];
+            i += 1;
+            row.get_text(1, &mut buf).unwrap();
+            assert_eq!(expected_row[0], std::str::from_utf8(&buf).unwrap());
+            row.get_text(3, &mut buf).unwrap();
+            assert_eq!(expected_row[1], std::str::from_utf8(&buf).unwrap());
+            row.get_text(4, &mut buf).unwrap();
+            assert_eq!(expected_row[2], std::str::from_utf8(&buf).unwrap());
         }
     }
 }
