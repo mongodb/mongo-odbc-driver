c6884119a337d9b361e47bfd686ea8d9909310a8
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2022-12-16 16:53:32 -0500

    SQL-985: Improve wording

--- integration_test/src/bin/test_generator.rs
+++ integration_test/src/bin/test_generator.rs
@@ -1,10 +1,10 @@
 use integration_test::test_runner::{run_integration_tests, Result};
 
 /// This is a standalone executable that generates baseline integration
-/// test files based on the description, db, and the test_definition fields
+/// test files based on the description, db, and test_definition fields
 /// for all test cases in the resources/integration_test/testes directory.
 /// It does this by executing the integration tests and writing the results
-/// to a file instead of validating the results match expectation.
+/// to files instead of validating the results match expectation.
 ///
 /// The actual code for generating the test files can be found in the
 /// test_runner module.
