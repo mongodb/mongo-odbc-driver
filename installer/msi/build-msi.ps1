f11581616bec99e9399612f3a9981c42cfc1e786
Patrick Meredith <pmeredit@gmail.com>
2023-03-22 22:43:04 -0400

    SQL-1276: Fix path

--- installer/msi/build-msi.ps1
+++ installer/msi/build-msi.ps1
@@ -19,8 +19,9 @@ $sourceDir = pwd
 $resourceDir = pwd
 $binDir = pwd
 $objDIr = ".\objs\"
-#$WixPath = "C:\wixtools\bin\"
-$WixPath = "C:\Program Files (x86)\WiX Toolset v3.11\bin"
+$WixPath = "C:\wixtools\bin\"
+# for local building, most installations will be in the directory below
+#$WixPath = "C:\Program Files (x86)\WiX Toolset v3.11\bin"
 $wixUiExt = "$WixPath\WixUIExtension.dll"
 
 if (-not ($VersionLabel -match "(\d\.\d).*")) {
