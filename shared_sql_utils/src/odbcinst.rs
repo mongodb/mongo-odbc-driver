0cb230aa25369b3e906839fd73b3a294f1778fac
Nathan Leniz <nathan.leniz@mongodb.com>
2023-03-27 11:44:53 -0700

    shared_sql_utils/src/odbcinst:

--- shared_sql_utils/src/odbcinst.rs
+++ shared_sql_utils/src/odbcinst.rs
@@ -44,3 +44,25 @@ extern "C" {
     ) -> i32;
     pub fn SQLGetConfigMode(buffer: *mut u32) -> i32;
 }
+#[cfg(target_os = "macos")]
+#[link(name = "iodbcinst", kind = "dylib")]
+extern "C" {
+    pub fn SQLValidDSNW(dsn: *const WideChar) -> bool;
+    pub fn SQLWriteDSNToIniW(dsn: *const WideChar, driver: *const WideChar) -> bool;
+    pub fn SQLWritePrivateProfileStringW(
+        section: *const WideChar,
+        entry: *const WideChar,
+        string: *const WideChar,
+        filename: *const WideChar,
+    ) -> bool;
+    pub fn SQLRemoveDSNFromIniW(dsn: *const WideChar) -> bool;
+    pub fn SQLGetPrivateProfileStringW(
+        section: *const WideChar,
+        entry: *const WideChar,
+        default: *const WideChar,
+        buffer: *mut WideChar,
+        buffer_size: i32,
+        filename: *const WideChar,
+    ) -> i32;
+    pub fn SQLGetConfigMode(buffer: *mut u32) -> i32;
+}
