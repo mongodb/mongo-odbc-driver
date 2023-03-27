8b762e0df504f593d603fcac23050ff290626c22
Nathan <nathan.leniz@mongodb.com>
2023-03-27 11:26:48 -0700

    add linux sql installer library path

--- shared_sql_utils/src/lib.rs
+++ shared_sql_utils/src/lib.rs
@@ -1,6 +1,3 @@
-#[cfg(target_os = "windows")]
 mod dsn;
-#[cfg(target_os = "windows")]
 pub mod odbcinst;
-#[cfg(target_os = "windows")]
 pub use dsn::DSNOpts;
--- shared_sql_utils/src/odbcinst.rs
+++ shared_sql_utils/src/odbcinst.rs
@@ -22,3 +22,25 @@ extern "C" {
     ) -> i32;
     pub fn SQLGetConfigMode(buffer: *mut u32) -> i32;
 }
+#[cfg(target_os = "linux")]
+#[link(name = "odbcinst", kind = "dylib")]
+extern "C" {
+    pub fn SQLValidDSNW(dsn: *const WideChar) -> bool;
+    pub fn SQLWriteDSNToIniW(dsn: *const WideChar, driver: *const WideChar) -> bool;
+    pub fn SQLWritePrivateProfileStringW(
+        section: *const WideChar,
+        entry: *const WideChar,
+        string: *const WideChar,
+        filename: *const WideChar,
+    ) -> bool;
+    pub fn SQLRemoveDSNFromIniW(dsn: *const WideChar) -> bool;
+    pub fn SQLGetPrivateProfileStringW(
+        section: *const WideChar,
+        entry: *const WideChar,
+        default: *const WideChar,
+        buffer: *mut WideChar,
+        buffer_size: i32,
+        filename: *const WideChar,
+    ) -> i32;
+    pub fn SQLGetConfigMode(buffer: *mut u32) -> i32;
+}
