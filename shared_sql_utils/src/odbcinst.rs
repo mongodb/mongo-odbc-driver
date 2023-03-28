575dafb4f426a860d34b395712e55c2046ec6769
= <=>
2023-03-27 21:04:21 -0700

    use cfg_attr for fn defs

--- shared_sql_utils/src/odbcinst.rs
+++ shared_sql_utils/src/odbcinst.rs
@@ -1,51 +1,8 @@
 use cstr::WideChar;
 
-#[cfg(target_os = "windows")]
-#[link(name = "odbccp32", kind = "raw-dylib")]
-extern "C" {
-    pub fn SQLValidDSNW(dsn: *const WideChar) -> bool;
-    pub fn SQLWriteDSNToIniW(dsn: *const WideChar, driver: *const WideChar) -> bool;
-    pub fn SQLWritePrivateProfileStringW(
-        section: *const WideChar,
-        entry: *const WideChar,
-        string: *const WideChar,
-        filename: *const WideChar,
-    ) -> bool;
-    pub fn SQLRemoveDSNFromIniW(dsn: *const WideChar) -> bool;
-    pub fn SQLGetPrivateProfileStringW(
-        section: *const WideChar,
-        entry: *const WideChar,
-        default: *const WideChar,
-        buffer: *mut WideChar,
-        buffer_size: i32,
-        filename: *const WideChar,
-    ) -> i32;
-    pub fn SQLGetConfigMode(buffer: *mut u32) -> i32;
-}
-#[cfg(target_os = "linux")]
-#[link(name = "odbcinst", kind = "dylib")]
-extern "C" {
-    pub fn SQLValidDSNW(dsn: *const WideChar) -> bool;
-    pub fn SQLWriteDSNToIniW(dsn: *const WideChar, driver: *const WideChar) -> bool;
-    pub fn SQLWritePrivateProfileStringW(
-        section: *const WideChar,
-        entry: *const WideChar,
-        string: *const WideChar,
-        filename: *const WideChar,
-    ) -> bool;
-    pub fn SQLRemoveDSNFromIniW(dsn: *const WideChar) -> bool;
-    pub fn SQLGetPrivateProfileStringW(
-        section: *const WideChar,
-        entry: *const WideChar,
-        default: *const WideChar,
-        buffer: *mut WideChar,
-        buffer_size: i32,
-        filename: *const WideChar,
-    ) -> i32;
-    pub fn SQLGetConfigMode(buffer: *mut u32) -> i32;
-}
-#[cfg(target_os = "macos")]
-#[link(name = "iodbcinst", kind = "dylib")]
+#[cfg_attr(target_os = "linux", link(name = "odbcinst", kind = "dylib"))]
+#[cfg_attr(target_os = "macos", link(name = "iodbcinst", kind = "dylib"))]
+#[cfg_attr(target_os = "windows", link(name = "odbc32", kind = "raw-dylib"))]
 extern "C" {
     pub fn SQLValidDSNW(dsn: *const WideChar) -> bool;
     pub fn SQLWriteDSNToIniW(dsn: *const WideChar, driver: *const WideChar) -> bool;
