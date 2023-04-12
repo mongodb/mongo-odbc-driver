f8836d2174d0c6b46803638d04d38ac28fb3164a
= <=>
2023-04-12 11:14:48 -0700

    drop handle to enforce log flush

--- integration_test/tests/connection_tests.rs
+++ integration_test/tests/connection_tests.rs
@@ -6,22 +6,24 @@ mod integration {
 
     #[test]
     fn test_invalid_connection() {
-        let env_handle = allocate_env();
+        let env_handle = allocate_env().unwrap();
         // Missing PWD
         let conn_str = "Driver=MongoDB Atlas SQL ODBC Driver;USER=N_A;SERVER=N_A";
-        let result = connect_with_conn_string(env_handle.unwrap(), conn_str.to_string());
+        let result = connect_with_conn_string(env_handle, conn_str.to_string());
 
         assert!(
             result.is_err(),
             "The connection should have failed, but it was successful."
         );
+        let _ = unsafe { Box::from_raw(env_handle) };
     }
 
     #[test]
     fn test_default_connection() {
-        let env_handle = allocate_env();
+        let env_handle = allocate_env().unwrap();
         let conn_str = crate::common::generate_default_connection_str();
-        let _ = connect_with_conn_string(env_handle.unwrap(), conn_str).unwrap();
+        let _ = connect_with_conn_string(env_handle, conn_str).unwrap();
+        let _ = unsafe { Box::from_raw(env_handle) };
     }
 
     #[cfg(test)]
@@ -59,13 +61,14 @@ mod integration {
 
             let _ = fs::remove_file(&log_file_path);
 
-            let env_handle = allocate_env();
+            let env_handle = allocate_env().unwrap();
             let conn_str = format!(
                 "{}{}",
                 crate::common::generate_default_connection_str(),
                 "loglevel=debug",
             );
-            connect_with_conn_string(env_handle.unwrap(), conn_str).unwrap();
+            connect_with_conn_string(env_handle, conn_str).unwrap();
+            let _ = unsafe { Box::from_raw(env_handle) };
 
             let log = fs::read_to_string(log_file_path).unwrap();
 
@@ -83,20 +86,22 @@ mod integration {
         use crate::common::{allocate_env, connect_with_conn_string};
         #[test]
         fn test_valid_dsn_connection() {
-            let env_handle = allocate_env();
+            let env_handle = allocate_env().unwrap();
             let conn_str = "DSN=ADF_Test";
-            connect_with_conn_string(env_handle.unwrap(), conn_str.to_string()).unwrap();
+            connect_with_conn_string(env_handle, conn_str.to_string()).unwrap();
+            let _ = unsafe { Box::from_raw(env_handle) };
         }
 
         #[test]
         fn test_uri_opts_override_dsn() {
-            let env_handle = allocate_env();
+            let env_handle = allocate_env().unwrap();
             let conn_str = "PWD=wrong;DSN=ADF_Test";
-            let result = connect_with_conn_string(env_handle.unwrap(), conn_str.to_string());
+            let result = connect_with_conn_string(env_handle, conn_str.to_string());
             assert!(
                 result.is_err(),
                 "The connection should have failed, but it was successful."
             );
+            let _ = unsafe { Box::from_raw(env_handle) };
         }
     }
 }
