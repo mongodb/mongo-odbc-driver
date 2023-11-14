f1e636c6f9f8fde3dff71b6c38b7a2098a820fa1
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2023-11-14 16:55:32 -0500

    SQL-1265: Use shutdown method on client before dropping reference

--- odbc/src/api/functions.rs
+++ odbc/src/api/functions.rs
@@ -852,6 +852,14 @@ pub unsafe extern "C" fn SQLDisconnect(connection_handle: HDbc) -> SqlReturn {
             let conn = must_be_valid!((*conn_handle).as_connection());
             // set the mongo_connection to None. This will cause the previous mongo_connection
             // to drop and disconnect.
+            conn.mongo_connection
+                .write()
+                .unwrap()
+                .as_ref()
+                .unwrap()
+                .client
+                .clone()
+                .shutdown();
             *conn.mongo_connection.write().unwrap() = None;
             SqlReturn::SUCCESS
         },
