a0a7af23355d0270a38b7e12b1abac65b5f47230
Ethan Hardy <ethan.hardy@mongodb.com>
2024-03-04 15:54:46 -0600

    implement function and error handling

--- odbc/src/api/functions.rs
+++ odbc/src/api/functions.rs
@@ -15,10 +15,10 @@ use constants::*;
 use cstr::{input_text_to_string_w, Charset, WideChar};
 
 use definitions::{
-    AsyncEnable, AttrConnectionPooling, AttrCpMatch, AttrOdbcVersion, CDataType, Concurrency,
-    ConnectionAttribute, CursorScrollable, CursorSensitivity, CursorType, Desc, DiagType,
-    DriverConnectOption, EnvironmentAttribute, FetchOrientation, HDbc, HDesc, HEnv, HStmt, HWnd,
-    Handle, HandleType, InfoType, Integer, Len, NoScan, Nullability, Pointer, RetCode,
+    AsyncEnable, AttrConnectionPooling, AttrCpMatch, AttrOdbcVersion, BindType, CDataType,
+    Concurrency, ConnectionAttribute, CursorScrollable, CursorSensitivity, CursorType, Desc,
+    DiagType, DriverConnectOption, EnvironmentAttribute, FetchOrientation, HDbc, HDesc, HEnv,
+    HStmt, HWnd, Handle, HandleType, InfoType, Integer, Len, NoScan, Nullability, Pointer, RetCode,
     RetrieveData, SmallInt, SqlBool, SqlDataType, SqlReturn, StatementAttribute, ULen, USmallInt,
     UseBookmarks,
 };
@@ -337,13 +337,80 @@ fn sql_alloc_handle(
 #[no_mangle]
 pub unsafe extern "C" fn SQLBindCol(
     hstmt: HStmt,
-    _col_number: USmallInt,
-    _target_type: SmallInt,
-    _target_value: Pointer,
-    _buffer_length: Len,
-    _length_or_indicatior: *mut Len,
+    col_number: USmallInt,
+    target_type: SmallInt,
+    target_value: Pointer,
+    buffer_length: Len,
+    length_or_indicator: *mut Len,
 ) -> SqlReturn {
-    unimpl!(hstmt);
+    panic_safe_exec_keep_diagnostics!(
+        debug,
+        || {
+            let mongo_handle = MongoHandleRef::from(hstmt);
+            let stmt = must_be_valid!((*mongo_handle).as_statement());
+
+            // Currently, we only support column binding of one row at a time.
+            if stmt.attributes.read().unwrap().row_array_size != 1
+                || !stmt
+                    .attributes
+                    .read()
+                    .unwrap()
+                    .row_bind_offset_ptr
+                    .is_null()
+                || stmt.attributes.read().unwrap().row_bind_type != BindType::SQL_BIND_BY_COLUMN
+            {
+                return unsupported_function!(hstmt);
+            }
+
+            // makes sure that col_number is in bounds
+            let mongo_stmt = stmt.mongo_statement.read().unwrap();
+            if mongo_stmt.is_none() {
+                stmt.errors.write().unwrap().push(ODBCError::NoResultSet);
+                return SqlReturn::ERROR;
+            }
+
+            let col_max_size = mongo_stmt.as_ref().unwrap().get_resultset_metadata().len();
+
+            // columns are 1-indexed as per the ODBC spec.
+            if (col_number as usize) > col_max_size || col_number <= 0 {
+                add_diag_info!(stmt_handle, ODBCError::InvalidColumnNumber(col_number));
+                return SqlReturn::ERROR;
+            }
+
+            if stmt.bound_cols.read().unwrap().is_none() {
+                *stmt.bound_cols.write().unwrap() = Some(HashMap::new());
+            }
+
+            // Unbind column if target_value is null
+            if target_value.is_null() {
+                stmt.bound_cols
+                    .write()
+                    .unwrap()
+                    .as_mut()
+                    .unwrap()
+                    .remove(&col_number);
+            }
+            // Bind column
+            else {
+                let bound_col_info = BoundColInfo {
+                    target_type,
+                    target_buffer: target_value,
+                    buffer_length,
+                    length_or_indicator,
+                };
+
+                stmt.bound_cols
+                    .write()
+                    .unwrap()
+                    .as_mut()
+                    .unwrap()
+                    .insert(col_number, bound_col_info);
+            }
+
+            SqlReturn::SUCCESS
+        },
+        hstmt
+    );
 }
 
 ///
@@ -1163,7 +1230,40 @@ unsafe fn sql_fetch_helper(statement_handle: HStmt, function_name: &str) -> SqlR
 
         *stmt.var_data_cache.write().unwrap() = Some(HashMap::new());
 
-        if !warnings_opt.is_empty() {
+        let mut success_with_info_encountered = false;
+
+        if stmt.bound_cols.read().unwrap().is_some() {
+            let mongo_handle_for_sql_get_data_helper = MongoHandleRef::from(statement_handle);
+
+            for (col, bound_col_info) in stmt.bound_cols.read().unwrap().as_ref().unwrap().iter() {
+                let sql_return = match FromPrimitive::from_i16(bound_col_info.target_type) {
+                    Some(valid_type) => sql_get_data_helper(
+                        mongo_handle_for_sql_get_data_helper,
+                        *col,
+                        valid_type,
+                        bound_col_info.target_buffer,
+                        bound_col_info.buffer_length,
+                        bound_col_info.length_or_indicator,
+                    ),
+                    None => {
+                        add_diag_info!(
+                            mongo_handle,
+                            ODBCError::InvalidTargetType(bound_col_info.target_type)
+                        );
+                        SqlReturn::ERROR
+                    }
+                };
+
+                match sql_return {
+                    SqlReturn::ERROR => return SqlReturn::ERROR,
+                    SqlReturn::SUCCESS_WITH_INFO => {
+                        success_with_info_encountered = true;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        if !warnings_opt.is_empty() || success_with_info_encountered {
             // No warnings and there is a next row
             SqlReturn::SUCCESS_WITH_INFO
         } else {
