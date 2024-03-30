31d4f8cbafdfcfa174b18bea05f3a5abe4572370
Patrick Meredith <pmeredit@gmail.com>
2024-03-30 12:19:33 -0400

    SQL-1934: Missing unwrap in tokio main

--- rfc8252_http_server/src/bin/tokio_rfc8252_http_server.rs
+++ rfc8252_http_server/src/bin/tokio_rfc8252_http_server.rs
@@ -3,7 +3,7 @@ use rfc8252_http_server::tokio_start;
 #[tokio::main]
 async fn main() {
     let (server, mut oidc_params) = tokio_start().await;
-    let res = oidc_params.recv().await;
+    let res = oidc_params.recv().await.unwrap();
     server.stop(true).await;
     println!("server result: {:?}", res);
 }
