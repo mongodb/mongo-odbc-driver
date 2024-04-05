dee440079183a32872a782081321ad3770f5cc93
Patrick Meredith <pmeredit@gmail.com>
2024-04-05 15:37:47 -0400

    SQL-1935: Update to new SQL-1934 server

--- rfc8252_http_server/src/lib.rs
+++ rfc8252_http_server/src/lib.rs
@@ -1,24 +1,40 @@
 use actix_web::{
-    self, dev::ServerHandle, http, middleware, rt, web, App, HttpRequest, HttpResponse, HttpServer,
-    Result,
+    self, dev::ServerHandle, http, web, App, HttpRequest, HttpResponse, HttpServer, Result,
 };
 use askama::Template;
+use std::collections::HashMap;
 use std::result::Result as StdResult;
-use std::{collections::HashMap, sync::mpsc, thread};
-use tokio::sync::mpsc as tokio_mpsc;
+use tokio::sync::mpsc;
 
+// server constants
 const DEFAULT_REDIRECT_PORT: u16 = 27097;
 #[cfg(test)]
 const DEFAULT_REDIRECT_URI: &str = "http://localhost:27097/redirect";
+
+// server endpoints
+const ACCEPTED_ENDPOINT: &str = "/accepted";
+const CALLBACK_ENDPOINT: &str = "/callback";
+const REDIRECT_ENDPOINT: &str = "/redirect";
+
+// OIDC response parameters
+const CODE: &str = "code";
+const ERROR: &str = "error";
+const ERROR_DESCRIPTION: &str = "error_description";
+const STATE: &str = "state";
+
+// html template constants
+// TODO SQL-2008: make sure this page exists and possibly update the link if the
+// docs team has a preference
+const ERROR_URI: &str = "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect/oidc_login_error";
+const PRODUCT_DOCS_LINK: &str =
+    "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect";
+const PRODUCT_DOCS_NAME: &str = "Atlas SQL ODBC Driver";
 
 #[derive(Template)]
 #[template(path = "OIDCAcceptedTemplate.html")]
 struct OIDCAcceptedPage<'a> {
     product_docs_link: &'a str,
     product_docs_name: &'a str,
-    error: &'a str,
-    error_uri: &'a str,
-    error_description: &'a str,
 }
 
 #[derive(Template)]
@@ -50,6 +66,7 @@ pub struct OidcResponseParams {
     pub state: Option<String>,
 }
 
+// get the parameters from the query string as a HashMap
 async fn get_params(query: &str) -> Result<HashMap<&str, &str>> {
     let params_vec: Vec<_> = query.split('&').collect();
     if params_vec.is_empty() || params_vec.first().unwrap().is_empty() {
@@ -70,7 +87,8 @@ async fn get_params(query: &str) -> Result<HashMap<&str, &str>> {
         .collect::<HashMap<&str, &str>>())
 }
 
-async fn threaded_callback(
+// Implement the callback url action
+async fn callback(
     oidc_params_sender: mpsc::Sender<StdResult<OidcResponseParams, String>>,
     req: HttpRequest,
 ) -> Result<HttpResponse> {
@@ -78,64 +96,31 @@ async fn threaded_callback(
     let params = match get_params(query).await {
         Ok(params) => params,
         Err(e) => {
-            return threaded_error(oidc_params_sender, "unknown error", &format!("{}", e)).await;
+            return error(oidc_params_sender, "parameters error", &format!("{}", e)).await;
         }
     };
 
-    let code = params.get("code");
+    let code = params.get(CODE);
     if let Some(code) = code {
-        let state = params.get("state").map(|s| s.to_string());
-        let oidc_response_params = OidcResponseParams {
-            code: code.to_string(),
-            state,
-        };
-        let _ = oidc_params_sender.send(Ok(oidc_response_params));
-        accepted().await
-    } else if let Some(e) = params.get("error") {
-        if let Some(error_description) = params.get("error_description") {
-            threaded_error(oidc_params_sender, e, error_description).await
-        } else {
-            threaded_error(oidc_params_sender, e, "no error description was provided").await
-        }
-    } else {
-        threaded_error(
-            oidc_params_sender,
-            "unknown error",
-            "response parameters are missing required information",
-        )
-        .await
-    }
-}
-
-async fn tokio_callback(
-    oidc_params_sender: tokio_mpsc::Sender<StdResult<OidcResponseParams, String>>,
-    req: HttpRequest,
-) -> Result<HttpResponse> {
-    let query = req.query_string();
-    let params = match get_params(query).await {
-        Ok(params) => params,
-        Err(e) => {
-            return tokio_error(oidc_params_sender, "unknown error", &format!("{}", e)).await;
-        }
-    };
-
-    let code = params.get("code");
-    if let Some(code) = code {
-        let state = params.get("state").map(|s| s.to_string());
+        let state = params.get(STATE).map(|s| s.to_string());
         let oidc_response_params = OidcResponseParams {
             code: code.to_string(),
             state,
         };
         let _ = oidc_params_sender.send(Ok(oidc_response_params)).await;
-        accepted().await
-    } else if let Some(e) = params.get("error") {
-        if let Some(error_description) = params.get("error_description") {
-            tokio_error(oidc_params_sender, e, error_description).await
+        // This will hide the code and state from the URL bar by doring a redirect
+        // to the /accepted page rather than rendering the accepted page directly
+        Ok(HttpResponse::Found()
+            .append_header((http::header::LOCATION, ACCEPTED_ENDPOINT))
+            .finish())
+    } else if let Some(e) = params.get(ERROR) {
+        if let Some(error_description) = params.get(ERROR_DESCRIPTION) {
+            error(oidc_params_sender, e, error_description).await
         } else {
-            tokio_error(oidc_params_sender, e, "no error description was provided").await
+            error(oidc_params_sender, e, "no error description was provided").await
         }
     } else {
-        tokio_error(
+        error(
             oidc_params_sender,
             "unknown error",
             "response parameters are missing required information",
@@ -144,46 +129,23 @@ async fn tokio_callback(
     }
 }
 
+// Implement the accepted page
 async fn accepted() -> Result<HttpResponse> {
     Ok(HttpResponse::build(http::StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body(
             OIDCAcceptedPage {
-                product_docs_link:
-                    "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect",
-                product_docs_name: "Atlas SQL ODBC Driver",
-                error: "error",
-                error_uri: "error_uri",
-                error_description: "error_description",
+                product_docs_link: PRODUCT_DOCS_LINK,
+                product_docs_name: PRODUCT_DOCS_NAME,
             }
             .render()
             .unwrap(),
         ))
 }
 
-async fn threaded_error(
+// Implement the error page and send the error on OIDC params channel
+async fn error(
     oidc_params_sender: mpsc::Sender<StdResult<OidcResponseParams, String>>,
-    error: &str,
-    error_description: &str,
-) -> Result<HttpResponse> {
-    let _ = oidc_params_sender.send(Err(format!("{}: {}", error, error_description)));
-    Ok(HttpResponse::build(http::StatusCode::BAD_REQUEST)
-        .content_type("text/html; charset=utf-8")
-        .body(
-            OIDCErrorPage {
-                product_docs_link: "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect",
-                product_docs_name: "Atlas SQL ODBC Driver",
-                error,
-                error_uri: "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect/bad_oidc_login",
-                error_description,
-            }
-            .render()
-            .unwrap(),
-        ))
-}
-
-async fn tokio_error(
-    oidc_params_sender: tokio_mpsc::Sender<StdResult<OidcResponseParams, String>>,
     error: &str,
     error_description: &str,
 ) -> Result<HttpResponse> {
@@ -194,10 +156,10 @@ async fn tokio_error(
         .content_type("text/html; charset=utf-8")
         .body(
             OIDCErrorPage {
-                product_docs_link: "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect",
-                product_docs_name: "Atlas SQL ODBC Driver",
+                product_docs_link: PRODUCT_DOCS_LINK,
+                product_docs_name: PRODUCT_DOCS_NAME,
                 error,
-                error_uri: "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect/bad_oidc_login",
+                error_uri: ERROR_URI,
                 error_description,
             }
             .render()
@@ -205,20 +167,22 @@ async fn tokio_error(
         ))
 }
 
+// Implement the not found page (404)
 async fn not_found() -> Result<HttpResponse> {
     Ok(HttpResponse::build(http::StatusCode::NOT_FOUND)
         .content_type("text/html; charset=utf-8")
         .body(
             OIDCNotFoundPage {
-                product_docs_link: "https://www.mongodb.com/docs/atlas/data-federation/query/sql/drivers/odbc/connect",
-                product_docs_name: "Atlas SQL ODBC Driver",
+                product_docs_link: PRODUCT_DOCS_LINK,
+                product_docs_name: PRODUCT_DOCS_NAME,
             }
             .render()
             .unwrap(),
         ))
 }
 
-async fn threaded_run_app(
+// The main runner for the server
+async fn run_app(
     sender: mpsc::Sender<ServerHandle>,
     oidc_params_sender: mpsc::Sender<StdResult<OidcResponseParams, String>>,
 ) -> std::io::Result<()> {
@@ -227,51 +191,19 @@ async fn threaded_run_app(
         let oidc_params_sender1 = oidc_params_sender.clone();
         let oidc_params_sender2 = oidc_params_sender.clone();
         App::new()
-            // enable logger
-            .wrap(middleware::Logger::default())
             .service(
-                web::resource("/callback")
-                    .to(move |r| threaded_callback(oidc_params_sender1.clone(), r)),
+                web::resource(CALLBACK_ENDPOINT)
+                    .to(move |r| callback(oidc_params_sender1.clone(), r)),
             )
             .service(
-                web::resource("/redirect")
-                    .to(move |r| threaded_callback(oidc_params_sender2.clone(), r)),
+                web::resource(REDIRECT_ENDPOINT)
+                    .to(move |r| callback(oidc_params_sender2.clone(), r)),
             )
+            .service(web::resource(ACCEPTED_ENDPOINT).to(accepted))
             .default_service(web::route().to(not_found))
     })
     .bind(("localhost", DEFAULT_REDIRECT_PORT))?
-    .workers(2)
-    .run();
-
-    // Send server handle back to the main thread
-    let _ = sender.send(server.handle());
-
-    server.await
-}
-
-async fn tokio_run_app(
-    sender: tokio_mpsc::Sender<ServerHandle>,
-    oidc_params_sender: tokio_mpsc::Sender<StdResult<OidcResponseParams, String>>,
-) -> std::io::Result<()> {
-    // srv is server controller type, `dev::Server`
-    let server = HttpServer::new(move || {
-        let oidc_params_sender1 = oidc_params_sender.clone();
-        let oidc_params_sender2 = oidc_params_sender.clone();
-        App::new()
-            // enable logger
-            .wrap(middleware::Logger::default())
-            .service(
-                web::resource("/callback")
-                    .to(move |r| tokio_callback(oidc_params_sender1.clone(), r)),
-            )
-            .service(
-                web::resource("/redirect")
-                    .to(move |r| tokio_callback(oidc_params_sender2.clone(), r)),
-            )
-            .default_service(web::route().to(not_found))
-    })
-    .bind(("localhost", DEFAULT_REDIRECT_PORT))?
-    .workers(2)
+    .workers(1)
     .run();
 
     // Send server handle back to the main thread
@@ -280,32 +212,17 @@ async fn tokio_run_app(
     server.await
 }
 
-pub fn threaded_start() -> (
+// The start function runs the main server runner in a tokio task and returns the server handle and
+// a receiver channel for the OIDC response parameters/errors
+pub async fn start() -> (
     ServerHandle,
     mpsc::Receiver<StdResult<OidcResponseParams, String>>,
 ) {
-    let (sender, receiver) = mpsc::channel();
-    let (oidc_params_sender, oidc_params_receiver) = mpsc::channel();
-
-    thread::spawn(move || {
-        let server_future = threaded_run_app(sender, oidc_params_sender);
-        rt::System::new().block_on(server_future)
-    });
-
-    let server_handle = receiver.recv().unwrap();
-
-    (server_handle, oidc_params_receiver)
-}
-
-pub async fn tokio_start() -> (
-    ServerHandle,
-    tokio_mpsc::Receiver<StdResult<OidcResponseParams, String>>,
-) {
-    let (sender, mut receiver) = tokio_mpsc::channel(1);
-    let (oidc_params_sender, oidc_params_receiver) = tokio_mpsc::channel(1);
+    let (sender, mut receiver) = mpsc::channel(1);
+    let (oidc_params_sender, oidc_params_receiver) = mpsc::channel(1);
 
     tokio::spawn(async move {
-        let server_future = tokio_run_app(sender, oidc_params_sender);
+        let server_future = run_app(sender, oidc_params_sender);
         server_future.await
     });
 
@@ -314,48 +231,10 @@ pub async fn tokio_start() -> (
     (server_handle, oidc_params_receiver)
 }
 
-#[test]
-fn rfc8252_http_server_threaded_accepted() {
+#[tokio::test(flavor = "current_thread")]
+async fn rfc8252_http_server_accepted() {
     use reqwest;
-    let (server_handle, oidc_params_receiver) = threaded_start();
-    let _ = reqwest::blocking::get(format!(
-        "{}{}",
-        DEFAULT_REDIRECT_URI, "/callback?code=1234&state=foo"
-    ))
-    .unwrap();
-    let oidc_params = oidc_params_receiver.recv().unwrap().unwrap();
-    rt::System::new().block_on(server_handle.stop(true));
-    assert_eq!(oidc_params.code, "1234");
-    assert_eq!(oidc_params.state, Some("foo".to_string()));
-}
-
-#[test]
-fn rfc8252_http_server_threaded_error() {
-    let (server_handle, oidc_params_receiver) = threaded_start();
-    let _ =
-        reqwest::blocking::get("http://localhost:9080/callback?error=1234&error_description=foo")
-            .unwrap();
-    let oidc_params = oidc_params_receiver.recv().unwrap();
-    rt::System::new().block_on(server_handle.stop(true));
-    assert_eq!(oidc_params, Err("1234: foo".to_string()));
-}
-
-#[test]
-fn rfc8252_http_server_threaded_no_params() {
-    let (server_handle, oidc_params_receiver) = threaded_start();
-    let _ = reqwest::blocking::get(DEFAULT_REDIRECT_URI).unwrap();
-    let oidc_params = oidc_params_receiver.recv().unwrap();
-    rt::System::new().block_on(server_handle.stop(true));
-    assert_eq!(
-        oidc_params,
-        Err("unknown error: response parameters are missing".to_string())
-    );
-}
-
-#[tokio::test]
-async fn rfc8252_http_server_tokio_accepted() {
-    use reqwest;
-    let (server_handle, mut oidc_params_receiver) = tokio_start().await;
+    let (server_handle, mut oidc_params_receiver) = start().await;
     let _ = reqwest::get(format!(
         "{}{}",
         DEFAULT_REDIRECT_URI, "?code=1234&state=foo"
@@ -368,9 +247,9 @@ async fn rfc8252_http_server_tokio_accepted() {
     assert_eq!(oidc_params.state, Some("foo".to_string()));
 }
 
-#[tokio::test]
-async fn rfc8252_http_server_tokio_error() {
-    let (server_handle, mut oidc_params_receiver) = tokio_start().await;
+#[tokio::test(flavor = "current_thread")]
+async fn rfc8252_http_server_error() {
+    let (server_handle, mut oidc_params_receiver) = start().await;
     let _ = reqwest::get(format!(
         "{}{}",
         DEFAULT_REDIRECT_URI, "?error=1234&error_description=foo"
@@ -382,14 +261,14 @@ async fn rfc8252_http_server_tokio_error() {
     assert_eq!(oidc_params, Err("1234: foo".to_string()));
 }
 
-#[tokio::test]
-async fn rfc8252_http_server_tokio_no_params() {
-    let (server_handle, mut oidc_params_receiver) = tokio_start().await;
+#[tokio::test(flavor = "current_thread")]
+async fn rfc8252_http_server_no_params() {
+    let (server_handle, mut oidc_params_receiver) = start().await;
     let _ = reqwest::get(DEFAULT_REDIRECT_URI).await.unwrap();
     let oidc_params = oidc_params_receiver.recv().await.unwrap();
     server_handle.stop(true).await;
     assert_eq!(
         oidc_params,
-        Err("unknown error: response parameters are missing".to_string())
+        Err("parameters error: response parameters are missing".to_string())
     );
 }
