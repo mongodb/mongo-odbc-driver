b433102afe79d45e651c2a705aa74e46b7aad55d
Patrick Meredith <pmeredit@gmail.com>
2024-04-17 18:23:30 -0400

    SSQL-1933: Add test bin

---
+++ core/src/bin/test_oidc_call_back.rs
@@ -0,0 +1,25 @@
+use mongo_odbc_core::oidc_auth::*;
+
+#[tokio::main]
+async fn main() {
+    let c = CallbackContext {
+        idp_info: Some(IdpServerInfo {
+            issuer: "https://mongodb-dev.okta.com/oauth2/ausqrxbcr53xakaRR357".to_string(),
+            client_id: Some("0oarvap2r7PmNIBsS357".to_string()),
+            // show that we get a refresh_token even when we don't ask for it here
+            request_scopes: Some(vec!["openid".to_string()]),
+        }),
+        refresh_token: None,
+        timeout_seconds: None,
+        version: 1,
+    };
+    let mut refresh_c = c.clone();
+    let IdpServerResponse {
+        access_token,
+        expires: _,
+        refresh_token,
+    } = oidc_call_back(c).await.unwrap();
+    println!("initial access token: {:access_token?}");
+    refresh_c.refresh_token = refresh_token;
+    println!("second response: {:?}", oidc_call_back(refresh_c).await.unwrap());
+}
