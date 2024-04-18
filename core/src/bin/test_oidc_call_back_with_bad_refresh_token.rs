ffb83f47155a444f63a8c4365f50a471986afb21
Patrick Meredith <pmeredit@gmail.com>
2024-04-18 19:33:18 -0400

    SQL-1933: Add comment

--- core/src/bin/test_oidc_call_back_with_bad_refresh_token.rs
+++ core/src/bin/test_oidc_call_back_with_bad_refresh_token.rs
@@ -14,6 +14,9 @@ async fn main() {
         timeout_seconds: Some(std::time::Instant::now() + std::time::Duration::from_secs(2)),
         version: 1,
     };
+    // this should return something similar to: Some(Other("OpenID Connect: code exchange failed:
+    // Server returned error response: StandardErrorResponse { error: invalid_grant,
+    // error_description: Some(\"The refresh token is invalid or expired.\"), error_uri: None }"))
     println!(
         "This will result in an error: {:?}",
         oidc_call_back(c).await.unwrap_err().get_custom::<Error>()
