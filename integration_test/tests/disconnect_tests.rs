6e658aa04c8371787b86273b0246223497b6d1d8
Nathan Leniz <nathan.leniz@gmail.com>
2024-04-22 12:57:05 -0700

    improve tests

--- integration_test/tests/disconnect_tests.rs
+++ integration_test/tests/disconnect_tests.rs
@@ -1,6 +1,8 @@
 mod common;
 
 mod integration {
+    use std::ptr;
+
     use crate::common::{
         allocate_env, allocate_statement, connect_with_conn_string,
         default_setup_connect_and_alloc_stmt, disconnect_and_free_dbc_and_env_handles,
@@ -20,10 +22,13 @@ mod integration {
                 SqlReturn::SUCCESS,
                 SQLExecDirectW(stmt, query.as_ptr(), SQL_NTS as i32,),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt.cast::<Handle>()),
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt).cast::<Handle>()
+                ),
             );
 
-            // we will not free the statement, instead proceeding directly to disconnect and freeing the dbc and env handles
+            // stmt isn't freed here, but it should be closed when the DBC is disconnected
             disconnect_and_free_dbc_and_env_handles(env, dbc)
         }
     }
@@ -37,17 +42,19 @@ mod integration {
                 SqlReturn::SUCCESS,
                 SQLExecDirectW(stmt, query.as_ptr(), SQL_NTS as i32,),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt.cast::<Handle>()),
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt).cast::<Handle>()
+                ),
             );
 
             assert_eq!(
                 SqlReturn::SUCCESS,
                 SQLFreeStmt(stmt, FreeStmtOption::SQL_CLOSE as i16),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt.cast::<Handle>()),
+                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, stmt as *mut _),
             );
 
-            // we will not free the statement, instead proceeding directly to disconnect and freeing the dbc and env handles
             disconnect_and_free_dbc_and_env_handles(env, dbc)
         }
     }
@@ -61,18 +68,23 @@ mod integration {
                 SqlReturn::SUCCESS,
                 SQLExecDirectW(stmt, query.as_ptr(), SQL_NTS as i32,),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt.cast::<Handle>()),
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt).cast::<Handle>()
+                ),
             );
 
-            // we didn't actually bind a statement, so this should be a no-op
+            // Should be a no-op since the statement is not bound. This will hang if SQLDisconnect does not close the cursor
             assert_eq!(
                 SqlReturn::SUCCESS,
                 SQLFreeStmt(stmt, FreeStmtOption::SQL_UNBIND as i16),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt.cast::<Handle>()),
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt).cast::<Handle>()
+                ),
             );
 
-            // we will not free the statement, instead proceeding directly to disconnect and freeing the dbc and env handles
             disconnect_and_free_dbc_and_env_handles(env, dbc)
         }
     }
@@ -82,12 +94,16 @@ mod integration {
         let env = allocate_env(AttrOdbcVersion::SQL_OV_ODBC3);
         let dbc = connect_with_conn_string(env, None)
             .expect("Failed to connect with default connection string");
+        // The following statements will be allocated and interacted with in the following ways:
+        // will be closed
         let stmt_1 = allocate_statement(dbc).expect("Failed to allocate statement 1");
+        // will be unbound
         let stmt_2 = allocate_statement(dbc).expect("Failed to allocate statement 2");
+        // will be closed and have its handle deallocated
         let stmt_3 = allocate_statement(dbc).expect("Failed to allocate statement 3");
+        // will have no operations performed on it
         let stmt_4 = allocate_statement(dbc).expect("Failed to allocate statement 4");
-        let stmt_5 = allocate_statement(dbc).expect("Failed to allocate statement 5");
-        let statements = [stmt_1, stmt_2, stmt_3, stmt_4, stmt_5];
+        let statements = [stmt_1, stmt_2, stmt_3, stmt_4];
 
         let query = b"SELECT * FROM integration_test.foo\0".map(|b| b as u16);
         statements.iter().for_each(|stmt| unsafe {
@@ -95,33 +111,61 @@ mod integration {
                 SqlReturn::SUCCESS,
                 SQLExecDirectW(*stmt, query.as_ptr(), SQL_NTS as i32,),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt.cast::<Handle>()),
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt).cast::<Handle>()
+                ),
             );
         });
 
-        // we will close statements 1 and 3
         unsafe {
+            // Close statement 1
             assert_eq!(
                 SqlReturn::SUCCESS,
                 SQLFreeStmt(stmt_1, FreeStmtOption::SQL_CLOSE as i16),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt_1.cast::<Handle>()),
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt_1).cast::<Handle>()
+                ),
             );
 
+            // Unbind statement 2
+            assert_eq!(
+                SqlReturn::SUCCESS,
+                SQLFreeStmt(stmt_2, FreeStmtOption::SQL_UNBIND as i16),
+                "{}",
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt_2).cast::<Handle>()
+                ),
+            );
+
+            // Close statement 3
             assert_eq!(
                 SqlReturn::SUCCESS,
                 SQLFreeStmt(stmt_3, FreeStmtOption::SQL_CLOSE as i16),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt_3.cast::<Handle>()),
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt_3).cast::<Handle>()
+                ),
             );
 
-            // and we'll free statement 3 handle
+            // Free the handle for statement 3
             assert_eq!(
                 SqlReturn::SUCCESS,
-                // interestingly, rust doesn't like the cast here, so we'll just use the raw value
-                SQLFreeHandle(HandleType::SQL_HANDLE_STMT as i16, stmt_3 as *mut _),
+                // Rust does not like the *stmt_3.cast::<Handle>() syntax, so we have to use
+                // as *mut _
+                SQLFreeHandle(
+                    HandleType::SQL_HANDLE_STMT as i16,
+                    *ptr::addr_of!(stmt_3).cast::<Handle>()
+                ),
                 "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, *stmt_3.cast::<Handle>())
+                get_sql_diagnostics(
+                    HandleType::SQL_HANDLE_STMT,
+                    *ptr::addr_of!(stmt_3).cast::<Handle>()
+                ),
             );
         }
 
