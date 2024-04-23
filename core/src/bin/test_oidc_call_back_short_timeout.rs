31815f8068a31e1cd05b23a98556383326941ef3
Patrick Meredith <pmeredit@protonmail.com>
2024-04-23 12:02:25 -0400

    SQL-1933: Add oidc_call_back function (#206)
    * SQL-1933: Add callback framework
    
    * SSQL-1933: Add test bin
    
    * SQL-1933: Fix typo, add tester demonstrating that timeout works
    
    * Update core/src/oidc_auth.rs
    
    * SSQL-1933: Make sure the two oidc_call_back tests work with current_thread runtime
    
    * SQL-1933: Switch to timeout and add test point for a bad refresh token
    
    * SQL-1933: Remove copy pasted comments that do not make sense in tests designed to fail
    
    * SQL-1933: cargo fmt
    
    * Update core/src/bin/test_oidc_call_back.rs
    
    Co-authored-by: Oliver Bucaojit <oliver.bucaojit@mongodb.com>
    
    * SQL-1933: Add comment
    
    ---------
    
    Co-authored-by: Oliver Bucaojit <oliver.bucaojit@mongodb.com>

---
+++ core/src/bin/test_oidc_call_back_short_timeout.rs
@@ -0,0 +1,19 @@
+use mongo_odbc_core::oidc_auth::*;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let c = CallbackContext {
+        idp_info: Some(IdpServerInfo {
+            issuer: "https://mongodb-dev.okta.com/oauth2/ausqrxbcr53xakaRR357".to_string(),
+            client_id: Some("0oarvap2r7PmNIBsS357".to_string()),
+            request_scopes: Some(vec!["openid".to_string()]),
+        }),
+        refresh_token: None,
+        // 2 seconds works well on my laptop. 1 second results in the request dying before the
+        //   server can redirect
+        timeout_seconds: Some(std::time::Instant::now() + std::time::Duration::from_secs(2)),
+        version: 1,
+    };
+    println!("This is very timing dependent, but we should see a timeout, if we play with the value: {:?}", oidc_call_back(c).await.unwrap_err().get_custom::<Error>());
+    // example output from my laptop: `This is very timing dependent, but we should see a timeout, if we play with the value: Some(Timedout)`
+}
