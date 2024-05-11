77c6e2da9363a89f664bc6390e39685a80008f5b
Patrick Meredith <pmeredit@gmail.com>
2024-05-11 11:22:52 -0400

    SQL-1937: Move core back to master, now that oidc is public

--- odbc/src/lib.rs
+++ odbc/src/lib.rs
@@ -14,7 +14,7 @@ use std::mem::forget;
 #[allow(non_snake_case, unused_variables)]
 extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut ()) -> bool {
     match call_reason {
-        DLL_PROCESS_ATTACH => forget(LoadLibraryA("user32.dll")),
+        DLL_PROCESS_ATTACH => forget(LoadLibraryA("")),
         DLL_PROCESS_DETACH => (),
         _ => (),
     }
