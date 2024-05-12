3063ac3af3c7cf7d36cf99e66d01dc4ef0f8870d
Patrick Meredith <pmeredit@gmail.com>
2024-05-11 20:20:17 -0400

    SQL-1937: Remove DLLMain because it does not reduce the number of logins in power-bi, since power-bi is actually opening multiple processes

--- odbc/src/lib.rs
+++ odbc/src/lib.rs
@@ -2,22 +2,3 @@ mod api;
 mod handles;
 
 pub use api::*;
-
-#[cfg(target_os = "windows")]
-use windows::{Win32::Foundation::*, Win32::System::LibraryLoader::*};
-
-#[cfg(target_os = "windows")]
-use std::mem::forget;
-
-#[cfg(target_os = "windows")]
-#[no_mangle]
-#[allow(non_snake_case, unused_variables)]
-extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut ()) -> bool {
-    match call_reason {
-        DLL_PROCESS_ATTACH => forget(LoadLibraryA("")),
-        DLL_PROCESS_DETACH => (),
-        _ => (),
-    }
-
-    true
-}
