cfd531a62c0cc65ec9e06d441ea81973f4dd937a
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2024-09-04 10:12:09 -0700

    SQL-2296: Testing updates

--- core/src/load_library.rs
+++ core/src/load_library.rs
@@ -3,9 +3,25 @@ use std::env;
 use std::path::PathBuf;
 use std::sync::Once;
 
-const LIBRARY_NAME: &str = "libmongosqltranslate";
-const MOCK_LIBRARY_NAME: &str = "libmock_mongosqltranslate";
+const LIBRARY_NAME: &str = "mongosqltranslate";
+const MOCK_LIBRARY_NAME: &str = "mock_mongosqltranslate";
 
+#[cfg(target_os = "windows")]
+const LIBRARY_EXTENSION: &str = "dll";
+#[cfg(target_os = "macos")]
+const LIBRARY_EXTENSION: &str = "dylib";
+#[cfg(not(any(target_os = "windows", target_os = "macos")))]
+const LIBRARY_EXTENSION: &str = "so";
+
+#[cfg(target_os = "windows")]
+const LIBRARY_INSTALL_PATH: &str = if cfg!(target_arch = "x86_64") {
+    "C:\\Program Files\\MongoDB\\Atlas SQL ODBC Driver\\bin"
+} else {
+    "C:\\Program Files\\MongoDB\\ODBC\\bin"
+};
+
+#[cfg(target_os = "macos")]
+const LIBRARY_INSTALL_PATH: &str = "/Library/MongoDB/MongoDB Atlas SQL ODBC Driver/";
 
 static INIT: Once = Once::new();
 static mut LIBRARY_LOADED: bool = false;
@@ -13,37 +29,26 @@ static mut LIBRARY: Option<Library> = None;
 
 fn get_library_path() -> PathBuf {
     let lib_name = if cfg!(target_os = "windows") {
-            "libmongosqltranslate.dll"
-    } else if cfg!(target_os = "macos") {
-        "libmongosqltranslate.dylib"
+        format!("{}.{}", LIBRARY_NAME, LIBRARY_EXTENSION)
     } else {
-        "libmongosqltranslate.so"
+        format!("lib{}.{}", LIBRARY_NAME, LIBRARY_EXTENSION)
     };
-
-    let mut path = PathBuf::new();
-    if cfg!(target_os = "windows") {
-        path.push("C:\\Program Files\\MongoDB\\");
-    } else if cfg!(target_os = "macos") {
-        path.push("/Library/MongoDB/");
-    }
-
+    let mut path = PathBuf::from(LIBRARY_INSTALL_PATH);
     path.push(lib_name);
     path
 }
 
-fn get_library_path_test() -> PathBuf {
+// #[cfg(test)]
+fn get_mock_library_path() -> PathBuf {
     let lib_name = if cfg!(target_os = "windows") {
-        "libmock_mongosqltranslate.dll"
-    } else if cfg!(target_os = "macos") {
-        "libmock_mongosqltranslate.dylib"
+        format!("{}.{}", LIBRARY_NAME, LIBRARY_EXTENSION)
     } else {
-        "libmock_mongosqltranslate.so"
+        format!("lib{}.{}", MOCK_LIBRARY_NAME, LIBRARY_EXTENSION)
     };
-
-    // testing, move the library to the target/debug directory
-    let mut path = std::env::current_exe().unwrap();
-    path.pop();
-    path.pop();
+    let mut path = env::current_exe().unwrap();
+    path.pop(); // Remove the executable name
+    path.pop(); // Remove 'debug' or 'release'
+    path.push("deps"); // Go to the 'deps' directory where the library should be located
     path.push(lib_name);
     path
 }
@@ -51,7 +56,13 @@ fn get_library_path_test() -> PathBuf {
 pub fn load_library() -> Result<(), Box<dyn std::error::Error>> {
     INIT.call_once(|| {
         unsafe {
-            match Library::new(get_library_path_test()) {
+            let library_path = if cfg!(test) {
+                get_mock_library_path()
+            } else {
+                get_library_path()
+            };
+
+            match Library::new(library_path) {
                 Ok(lib) => {
                     LIBRARY = Some(lib);
                     LIBRARY_LOADED = true;
@@ -69,6 +80,7 @@ pub fn load_library() -> Result<(), Box<dyn std::error::Error>> {
         Err("Failed to load the library".into())
     }
 }
+
 
 fn is_library_loaded() -> bool {
     unsafe { LIBRARY_LOADED }
@@ -96,6 +108,7 @@ mod lib_loading_test {
 
     #[test]
     fn test_run_command() {
+        println!("Entering test_run_command_library");
         load_library().expect("Failed to load library");
         assert!(is_library_loaded());
 
@@ -114,6 +127,7 @@ mod lib_loading_test {
             Vec::from_raw_parts(result_ptr as *mut u8, result_len, result_cap)
         };
         let result_doc: Document = bson::from_slice(&result_vec).expect("Failed to deserialize result");
+        println!("Exiting test_run_command_library");
 
         assert_eq!(result_doc, test_doc);
     }
