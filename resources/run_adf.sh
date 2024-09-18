bda81bfa9c30c99900df8a48345a3f406489f771
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2024-09-18 09:16:30 -0700

    SQL-2299: Use github token for cloning instead of ssh (#241)
    * SQL-2299: Use github token for cloning instead of ssh
    
    * SQL-2299: Clear git config settings related to url
    
    * SQL-2299: Add config for local run

--- resources/run_adf.sh
+++ resources/run_adf.sh
@@ -49,9 +49,25 @@ GO="$GOBINDIR/go"
 
 PATH=$GOBINDIR:$PATH
 
+# GITHUB_TOKEN must be set for cloning 10gen/mongohouse repository from Evergreen hosts, and the
+# dependencies needed to build it.
+# If unset, it will default to using the SSH private key on the local system.
+if [[ ${GITHUB_TOKEN} ]]; then
+  # Clear git url configurations if they exist
+  git config --global --get-regexp '^url\.' | while read -r key _; do
+      git config --global --unset "$key"
+  done
+
+  MONGOHOUSE_URI=https://x-access-token:${GITHUB_TOKEN}@github.com/10gen/mongohouse.git
+  git config --global url."https://x-access-token:${GITHUB_TOKEN}@github.com/10gen/".insteadOf https://github.com/10gen/
+
+else
+  MONGOHOUSE_URI=git@github.com:10gen/mongohouse.git
+  git config --global url.git@github.com:.insteadOf https://github.com/
+fi
+
 MACHINE_ARCH=$(uname -m)
 LOCAL_INSTALL_DIR=$(pwd)/local_adf
-MONGOHOUSE_URI=git@github.com:10gen/mongohouse.git
 MONGO_DB_PATH=$LOCAL_INSTALL_DIR/test_db
 LOGS_PATH=$LOCAL_INSTALL_DIR/logs
 DB_CONFIG_PATH=$(pwd)/resources/integration_test/testdata/adf_db_config.json
@@ -389,7 +405,6 @@ if [ $ARG = $START ]; then
           MONGOHOUSE_DIR=$LOCAL_INSTALL_DIR/mongohouse
         fi
         # Install and start mongohoused
-        git config --global url.git@github.com:.insteadOf https://github.com/
         # Clone the mongohouse repo
         if [ ! -d "$MONGOHOUSE_DIR" ]; then
             git clone $MONGOHOUSE_URI $MONGOHOUSE_DIR
