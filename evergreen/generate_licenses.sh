d585d1a6527f6b56f3ff8f039c231ff062815521
Nathan Leniz <nathan.leniz@mongodb.com>
2025-01-21 09:57:36 -0800

    fix typos

--- evergreen/generate_licenses.sh
+++ evergreen/generate_licenses.sh
@@ -6,7 +6,7 @@ LICENSE_FILE_NAME="THIRD_PARTY_LICENSES.txt"
 cargo install cargo-bundle-licenses
 
 # Generate the license file
-cargo bundle-licenses --format yaml --output resources/licenses.yaml
+cargo-bundle-licenses --format yaml --output resources/licenses.yaml
 
 # Ensure a clean slate by deleting the existing THIRD_PARTY_LICENSES.txt file
 rm -f "$LICENSE_FILE_NAME"
@@ -14,3 +14,5 @@ rm -f "$LICENSE_FILE_NAME"
 # Concatenate the third_party_header.txt file and the licenses.yaml file to create the THIRD_PARTY_LICENSES.txt file
 cat resources/third_party_header.txt >"$LICENSE_FILE_NAME"
 cat resources/licenses.yaml >>"$LICENSE_FILE_NAME"
+
+rm -f resources/licenses.yaml
