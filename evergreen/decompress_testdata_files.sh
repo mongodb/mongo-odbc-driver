289ba77c8b5fe6ea225d624dca9a730305089254
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2025-06-02 15:43:14 -0400

    SQL-2766: Update script to remove existing testdata

--- evergreen/decompress_testdata_files.sh
+++ evergreen/decompress_testdata_files.sh
@@ -1,5 +1,9 @@
 cd resources/integration_test
 
+echo "Removing existing testdata..."
+rm -rf testdata/
+echo "Removal complete"
+
 echo "Decompressing integration test data..."
 tar -xzvf testdata.tar.gz
 echo "Decompression complete"
