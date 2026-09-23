e08c641fbbb090533dfef9e49374bffe090a8fa0
Nicholas Cioli <nicholascioli@users.noreply.github.com>
2026-09-23 18:15:21 -0400

    address feedback

--- integration_test/tests/direct_cluster_tests.rs
+++ integration_test/tests/direct_cluster_tests.rs
@@ -13,14 +13,14 @@ mod direct_cluster_tests {
     use serde_json::{Number, Value};
     use std::ptr;
 
-    #[test]
-    fn test_sql_prepare_and_sql_execute_with_valid_query_and_valid_schemas_created_in_atlas_infinite(
-    ) {
+    /// Helper method to run a simple test against the airbnb sample dataset at
+    /// the specified cluster.
+    fn run_prepare_execute_airbnb_test(cluster_name: &str) {
         let (env_handle, dbc, stmt) = default_setup_connect_and_alloc_stmt(
             AttrOdbcVersion::SQL_OV_ODBC3,
             Some(crate::common::generate_srv_style_connection_string(
                 "sample_airbnb",
-                "infinite",
+                cluster_name,
             )),
         );
 
@@ -62,51 +62,15 @@ mod direct_cluster_tests {
     }
 
     #[test]
+    fn test_sql_prepare_and_sql_execute_with_valid_query_and_valid_schemas_created_in_atlas_infinite(
+    ) {
+        run_prepare_execute_airbnb_test("infinite");
+    }
+
+    #[test]
     fn test_sql_prepare_and_sql_execute_with_valid_query_and_valid_schemas_created_in_direct_cluster_mode(
     ) {
-        let (env_handle, dbc, stmt) = default_setup_connect_and_alloc_stmt(
-            AttrOdbcVersion::SQL_OV_ODBC3,
-            Some(crate::common::generate_srv_style_connection_string(
-                "sample_airbnb",
-                "cluster0",
-            )),
-        );
-
-        unsafe {
-            let mut query: Vec<WideChar> = cstr::to_widechar_vec("SELECT property_type, room_type, bed_type, minimum_nights, maximum_nights FROM listingsAndReviews ORDER BY _id LIMIT 3");
-            query.push(0);
-
-            assert_eq!(
-                SqlReturn::SUCCESS,
-                SQLPrepareW(stmt as HStmt, query.as_ptr(), SQL_NTS),
-                "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, stmt as Handle)
-            );
-
-            let expected_column_metadata_values = create_expected_column_metadata();
-
-            get_column_attributes(stmt as Handle, 5, Some(expected_column_metadata_values));
-
-            assert_eq!(
-                SqlReturn::SUCCESS,
-                SQLExecute(stmt as HStmt),
-                "{}",
-                get_sql_diagnostics(HandleType::SQL_HANDLE_STMT, stmt as Handle)
-            );
-
-            let expected_column_values = create_expected_column_values();
-
-            fetch_and_get_data(
-                stmt as Handle,
-                Some(3),
-                vec![SqlReturn::SUCCESS; 5],
-                vec![CDataType::SQL_C_WCHAR; 5],
-                Some(expected_column_values),
-            );
-
-            disconnect_and_close_handles(dbc, stmt);
-        }
-        let _ = unsafe { Box::from_raw(env_handle) };
+        run_prepare_execute_airbnb_test("cluster0");
     }
 
     #[test]
